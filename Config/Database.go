0a2684a8a47836217eb0f9fe125b2e81b387804e
ericanthonywuu <eric.anthony@ajaib.co.id>
2022-11-22 03:06:05 +0700

    initial commit

--- Config/Database.go
+++ Config/Database.go
@@ -1,5 +1,15 @@
 package Config
 
-func Init() {
+import (
+	"database/sql"
+	"os"
+)
 
+func InitDB() *sql.DB {
+	db, err := sql.Open(os.Getenv("DBDRIVER"), os.Getenv("DBURL"))
+	if err != nil {
+		panic(err)
+	}
+
+	return db
 }
