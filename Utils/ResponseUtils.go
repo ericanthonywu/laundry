20c555c6241279164c288da48ec8a91a3225321a
Eric Anthony <eric@alviss.co>
2023-02-03 13:19:35 +0700

    update

--- Utils/ResponseUtils.go
+++ Utils/ResponseUtils.go
@@ -3,19 +3,17 @@ package Utils
 import (
 	"fmt"
 	"github.com/labstack/echo/v4"
-	"laundry/Constant/APIResponse"
 	"laundry/Model"
 	"net/http"
 )
 
-func errorResponse(err error, context string, httpStatus int) error {
-	message := context
+func errorResponse(err error, message string, httpStatus int) error {
 	if httpStatus >= 500 {
 		message = "failed to " + message
 		fmt.Println(message+": ", err)
 	}
 
-	return echo.NewHTTPError(httpStatus, message)
+	return echo.NewHTTPError(httpStatus, http.StatusText(httpStatus))
 }
 
 func JWTErrorResponse(err error) error {
@@ -31,7 +29,7 @@ func BadRequestResponseWithMessage(message string) error {
 }
 
 func BadRequestResponse() error {
-	return errorResponse(nil, "bad request", http.StatusBadRequest)
+	return BadRequestResponseWithMessage(http.StatusText(http.StatusBadRequest))
 }
 
 func OkResponseMessage(c echo.Context, message string, data interface{}) error {
@@ -39,5 +37,5 @@ func OkResponseMessage(c echo.Context, message string, data interface{}) error {
 }
 
 func OkResponse(c echo.Context, data interface{}) error {
-	return c.JSON(http.StatusOK, Model.NewDefaultResponse(APIResponse.DefaultMessageResponse, data))
+	return OkResponseMessage(c, http.StatusText(http.StatusOK), data)
 }
